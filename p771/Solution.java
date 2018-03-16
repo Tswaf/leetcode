a33a32d4ab4c997c5eb3e75753d1d65bbe5462af
wangtong <wangtong01@corp.netease.com>
2018-03-16 23:57:43 +0800

    p771

---
+++ p771/Solution.java
@@ -0,0 +1,24 @@
+package p771_Jewels_and_Stones;
+
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        boolean[] letterMap = new boolean[52];
+        int index = -1;
+        char c = 'a';
+        for(int i=0; i<J.length(); i++){
+            c = J.charAt(i);
+            index = c-'a'>=0 ? c-'a' : c-'A'+26;
+            letterMap[index] = true;
+        }
+
+        int count = 0;
+        for(int i=0; i<S.length(); i++){
+            c = S.charAt(i);
+            index = c-'a'>=0 ? c-'a' : c-'A'+26;
+            if(letterMap[index]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
